42d0e3154abdffaa9dfffc121dcbd0f616845e0c
fraluegut <franciscojavierluengo@outlook.com>
2020-04-22 21:55:43 +0200

    Initial commit

---
+++ Live_Graph_Covid19.py
@@ -0,0 +1,20 @@
+import random
+from itertools import count
+import tkinter
+import matplotlib
+import pandas as pd
+import matplotlib.pyplot as plt
+
+matplotlib.use('TkAgg')
+#plt.style.use('fivethirtyeight')
+
+x_vals = [0, 1, 2, 3, 4, 5]
+y_vals = [0, 1, 7, 3, 2, 5]
+
+plt.plot(x_vals, y_vals)
+plt.savefig("mygraph.png")
+
+plt.tight_layout()
+plt.show()
+
+
